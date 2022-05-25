38ab82e5bae17f88e61d5d846a31c77fc2d5bf10
Kevin Le <kevin@fullcycle.io>
2022-05-25 18:09:03 -0500

    tried some javascript

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,17 +1,27 @@
 fetch("https://jsonplaceholder.typicode.com/todos/") 
-.then(function(res) {
+.then((res) => {
     return res.json()
 })
 .then((toDoItems) => {
-    console.log(toDoItems);
+    // console.log(toDoItems);
     const result = document.getElementById('toDoItems');
     for (var i = 0; i < toDoItems.length; i++){
-    result.innerHTML += '<li>' +
+        result.innerHTML += '<li>' +
         '<span>' + toDoItems[i].id + '</span>' +
         '<h2>' + toDoItems[i].title + '</h2>' +
-        '<span>' + toDoItems[i].completed + '</p>' +
-    '</li>';
+        '<span id="completes">' + toDoItems[i].completed + '</p>' +
+        '</li>';
+    }
+    return toDoItems
+})
+.then((res) => {
+    for (var i = 0; i < res.length; i++){
+        let completedClr = res[i].completed
+        if( completedClr === false) {
+            document.getElementById('completes').style.color = 'red'
+        } else {
+            document.getElementById('completes').style.color = 'green'
+        }
     }
 })
-
 
